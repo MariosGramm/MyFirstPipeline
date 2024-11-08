5d4071f8c634934220f01a8940a17c7019cf0cea
MariosGramm <mariosgramm21@gmail.com>
2024-11-08 19:47:04 +0200

    Update jenkinsfile.groovy

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -1,8 +1,9 @@
 pipeline {
     agent any
     environment {
-        AWS_ACCESS_KEY = credentials('aws_access_key') 
+        AWS_ACCESS_KEY = credentials('94cd2fc7-c66d-44f6-b0f6-4f8522c6105a') 
         AWS_SECRET_KEY = credentials('aws_secret_key') 
+        AWS_REGION = credentials('AWS_REGION')
     }
     stages{
         stage('Terraform Init'){
@@ -22,13 +23,13 @@ pipeline {
         }
         stage('Terraform Plan'){
             steps{
-                bat 'terraform plan -var "access_key=${AWS_ACCESS_KEY}" -var "secret_key=${AWS_SECRET_KEY}"'
+                bat 'terraform plan -var "access_key=${AWS_ACCESS_KEY}" -var "secret_key=${AWS_SECRET_KEY}" var "region=$AWS_REGION"'
             }
         }
         stage('Terraform Apply'){
             steps{
                 input message : "Confirm Deployment?", ok:"Deploy"
-                bat 'terraform apply -var "access_key=${AWS_ACCESS_KEY}" -var "secret_key=${AWS_SECRET_KEY}" --auto-approve'
+                bat 'terraform apply -var "access_key=${AWS_ACCESS_KEY}" -var "secret_key=${AWS_SECRET_KEY}" var "region=$AWS_REGION" --auto-approve'
             }
         }
 
